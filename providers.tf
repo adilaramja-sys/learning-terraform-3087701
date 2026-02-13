67cf6e41aaf008c49cc03334c6dc20a74acfc05b
adilaramja-sys <adil.aramja@gmail.com>
2026-02-13 15:55:59 +0100

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -6,7 +6,7 @@ terraform {
   }
 }
 
-#provider "aws" {
-#  region  = "us-west-2"
-#  profile = "cloud-platform-solutions-dev"
-#}
+provider "aws" {
+  region  = "eu-central-1"
+  profile = "cloud-platform-solutions-dev"
+}
